8d174c134d8bbde7168d8808d3e29accc05767b4
Harry Chen <czy88840616@gmail.com>
2019-10-29 15:26:24 +0800

    fix: fix object required cache

--- src/generator/CommonGenerator.ts
+++ src/generator/CommonGenerator.ts
@@ -1,11 +1,14 @@
-import { CommonGeneratorOptions, CopyWalker, TemplatePackageConfig } from '../interface';
+import {
+  CommonGeneratorOptions,
+  CopyWalker,
+  TemplatePackageConfig,
+} from '../interface';
 import { isAbsolute, join } from 'path';
-import { dirExistsSync, fileExistsSync } from '../util/fs';
+import { dirExistsSync, fileExistsSync, readFileSync } from '../util/fs';
 import untildify from 'untildify';
 import emptyDir from 'empty-dir';
 
 export abstract class CommonGenerator {
-
   copyWalker: CopyWalker;
   templateUri: string;
   targetPath: string;
@@ -29,14 +32,15 @@ export abstract class CommonGenerator {
 
       if (fileExistsSync(templatePkg)) {
         try {
-          const pkg = require(templatePkg);
-          const config = pkg[ 'boilerplateConfig' ] as TemplatePackageConfig;
+          const pkg = readFileSync(templatePkg);
+          const config = pkg['boilerplateConfig'] as TemplatePackageConfig;
           if (config) {
-            config.replaceFile = config.replaceFile || [ 'README.md' ];
+            config.replaceFile = config.replaceFile || ['README.md'];
             if (typeof config.replaceFile === 'string') {
-              config.replaceFile = [ config.replaceFile ];
+              config.replaceFile = [config.replaceFile];
             }
-            config.replaceParameter = config.replaceParameter as string || 'index.js';
+            config.replaceParameter =
+              (config.replaceParameter as string) || 'index.js';
             config.root = join(templateRoot, config.root || 'boilerplate');
             config.rule = config.rule || [];
             this.templateConfig = config;
@@ -53,10 +57,13 @@ export abstract class CommonGenerator {
    * 获取参数列表
    */
   async getParameterList() {
-    const templateConfig = await this.getTemplateConfig() as TemplatePackageConfig;
+    const templateConfig = (await this.getTemplateConfig()) as TemplatePackageConfig;
     const templateRoot = this.getTemplatePath();
     if (templateConfig) {
-      const parameterFile = join(templateRoot, templateConfig.replaceParameter as string);
+      const parameterFile = join(
+        templateRoot,
+        templateConfig.replaceParameter as string
+      );
       if (fileExistsSync(parameterFile)) {
         return require(parameterFile);
       }
@@ -71,8 +78,8 @@ export abstract class CommonGenerator {
     const parameterList = await this.getParameterList();
     const defaultValue = {};
     for (const key in parameterList) {
-      if (parameterList[ key ][ 'default' ]) {
-        defaultValue[ key ] = parameterList[ key ][ 'default' ];
+      if (parameterList[key]['default']) {
+        defaultValue[key] = parameterList[key]['default'];
       }
     }
     return defaultValue;
@@ -89,7 +96,9 @@ export abstract class CommonGenerator {
       }
     }
 
-    let templateConfig = await this.getTemplateConfig() as Partial<TemplatePackageConfig>;
+    let templateConfig = (await this.getTemplateConfig()) as Partial<
+      TemplatePackageConfig
+    >;
     let templateRoot = this.getTemplatePath();
     const packageRoot = templateRoot;
     if (templateConfig) {
@@ -101,7 +110,7 @@ export abstract class CommonGenerator {
       templateConfig = {
         replaceParameter: 'index.js',
         root: templateRoot,
-        replaceFile: [ 'README.md' ],
+        replaceFile: ['README.md'],
         rule: [],
       };
     }
@@ -116,7 +125,8 @@ export abstract class CommonGenerator {
     });
 
     if (templateConfig.afterAll) {
-      const afterScript = isAbsolute(templateConfig.afterAll) ? require(templateConfig.afterAll)
+      const afterScript = isAbsolute(templateConfig.afterAll)
+        ? require(templateConfig.afterAll)
         : require(join(packageRoot, templateConfig.afterAll));
       if (afterScript && typeof afterScript === 'function') {
         await afterScript({
