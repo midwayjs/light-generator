78f3379ece81605720b6b00325308bd2b3a5c820
mxr103105@alibaba-inc.com <mxr103105@alibaba-inc.com>
2019-11-06 11:53:24 +0800

    ignoreRule 正则匹配增加额外规则 适配client/_aaa.json case

--- src/rule.ts
+++ src/rule.ts
@@ -8,17 +8,30 @@ const debug = require('debug')('generator');
  * 移除文件下划线
  * @param currentFilePath
  */
-export const ignoreRule = async (currentFilePath, copyRuleOptions: CopyRuleOptions) => {
-  if (/^_/.test(copyRuleOptions.targetRelativeFile)) {
+export const ignoreRule = async (
+  currentFilePath,
+  copyRuleOptions: CopyRuleOptions
+) => {
+  // add RegExp for client/_aaa.json case
+  if (
+    /^_/.test(copyRuleOptions.targetRelativeFile) ||
+    /^(.*)\/_/.test(copyRuleOptions.targetRelativeFile)
+  ) {
     if (copyRuleOptions.filenameMapping.has(currentFilePath)) {
       // 如果名字被修改过，拿新的名字去替换
       const newFilePath = copyRuleOptions.filenameMapping.get(currentFilePath);
-      const newName = join(copyRuleOptions.targetDir, relative(copyRuleOptions.targetDir, newFilePath).replace('_', ''));
+      const newName = join(
+        copyRuleOptions.targetDir,
+        relative(copyRuleOptions.targetDir, newFilePath).replace('_', '')
+      );
       await fse.rename(newFilePath, newName);
       copyRuleOptions.filenameMapping.set(currentFilePath, newName);
       debug(` * remove _ => ${newName}`);
     } else {
-      const newName = join(copyRuleOptions.targetDir, copyRuleOptions.targetRelativeFile.replace('_', ''));
+      const newName = join(
+        copyRuleOptions.targetDir,
+        copyRuleOptions.targetRelativeFile.replace('_', '')
+      );
       await fse.rename(currentFilePath, newName);
       copyRuleOptions.filenameMapping.set(currentFilePath, newName);
       debug(` * remove _ => ${newName}`);
@@ -33,11 +46,20 @@ const pattern = /\{\{(\w*[:]*[=]*\w+)\}\}(?!})/g;
  * @param currentFilePath
  * @param copyRuleOptions
  */
-export const replaceRule = async (currentFilePath, copyRuleOptions: CopyRuleOptions) => {
+export const replaceRule = async (
+  currentFilePath,
+  copyRuleOptions: CopyRuleOptions
+) => {
   const replaceArgs = copyRuleOptions.replaceParameter || {};
-  if (includeReplaceContent(copyRuleOptions.templateConfig.replaceFile, copyRuleOptions.targetRelativeFile)) {
+  if (
+    includeReplaceContent(
+      copyRuleOptions.templateConfig.replaceFile,
+      copyRuleOptions.targetRelativeFile
+    )
+  ) {
     // 如果当前文件在替换列表中，则进行内容替换
-    const contents = fse.readFileSync(currentFilePath, 'utf-8')
+    const contents = fse
+      .readFileSync(currentFilePath, 'utf-8')
       .replace(pattern, (match, key, value) => {
         debug(` * replace content key => ${key}`);
         return replaceArgs[key] || match;
@@ -48,10 +70,13 @@ export const replaceRule = async (currentFilePath, copyRuleOptions: CopyRuleOpti
 
   // 如果文件名需要替换
   if (new RegExp(pattern).test(currentFilePath)) {
-    const newFilePath = currentFilePath.replace(pattern, (match, key, value) => {
-      debug(` * replace filename key => ${key}`);
-      return replaceArgs[key];
-    });
+    const newFilePath = currentFilePath.replace(
+      pattern,
+      (match, key, value) => {
+        debug(` * replace filename key => ${key}`);
+        return replaceArgs[key];
+      }
+    );
     await fse.move(currentFilePath, newFilePath);
     // 一定要更新文件名，不然后续处理找不到
     copyRuleOptions.filenameMapping.set(currentFilePath, newFilePath);
@@ -59,14 +84,19 @@ export const replaceRule = async (currentFilePath, copyRuleOptions: CopyRuleOpti
 };
 
 // 匹配是否需要替换内容
-function includeReplaceContent(replaceFilePatternList: string[], targetRelativeFile) {
+function includeReplaceContent(
+  replaceFilePatternList: string[],
+  targetRelativeFile
+) {
   for (const pattern of replaceFilePatternList) {
     if (typeof pattern === 'string' && !/\*/.test(pattern)) {
       if (pattern === targetRelativeFile) {
         return true;
       }
     } else {
-      const result = minimatch(targetRelativeFile, pattern, { matchBase: true });
+      const result = minimatch(targetRelativeFile, pattern, {
+        matchBase: true,
+      });
       if (result) {
         return true;
       }
