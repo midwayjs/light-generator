d8d9d4ad061e1e74f591afecbc65d1dfdb59367e
Harry Chen <czy88840616@gmail.com>
2020-05-14 17:26:34 +0800

    fix: fix name

--- test/util.test.ts
+++ test/util.test.ts
@@ -4,7 +4,8 @@ import { renamePackageName } from '../src/util/';
 describe('/test/util.test.ts', () => {
   it('should transform package name', () => {
     assert(renamePackageName('@ali/midway') === 'ali-midway');
-    assert(renamePackageName('@ali.midway') === '@ali.midway');
+    assert(renamePackageName('@ali.midway') === 'ali.midway');
     assert(renamePackageName('@scope/midway') === 'scope-midway');
+    assert(renamePackageName('@midwayjs-examples/midway') === 'midwayjs-examples-midway');
   });
 });
